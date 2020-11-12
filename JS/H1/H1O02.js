1299c60beace4a4bdffa4c9e1e9f56e041b0eea5
NiekAppeldoorn <152606@csgaugustinus.nl>
2020-11-12 09:07:54 +0000

    sfgjgf

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -9,4 +9,26 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+
+  noStroke();
+  fill('darkred');
+  triangle(225,450,0,0,450,0);
+
+  noStroke();
+  fill('pink');
+  triangle(450,0,0,225,450,450);
+
+noStroke();
+  fill('purple');
+  triangle(0,450,0,0,450,225);
+
+strokeWeight(5);
+  stroke('pink');
+  fill('darkred');
+  beginShape();
+  vertex(225,115);
+  vertex(300,225);
+  vertex(225,335);
+  vertex(0,225);
+  endShape(CLOSE);
 }
